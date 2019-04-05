d15b74d606e60f6a387bcdbaadf3503f8f200dbf
Hannah Ingham <hannahingham@MacBooks-MacBook-Pro.local>
2019-04-05 09:17:26 -0700

    making items more readable, adding help-wanted options

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ export default function header() {
         <div class="container">
           <h1 class="title is-1">Issue First</h1>
           <h2 class="subtitle is-3">
-            Find Open Github Issues & Contribute to Open Source!
+            Search Github Issues & Contribute to Open Source!
           </h2>
         </div>
       </div>
